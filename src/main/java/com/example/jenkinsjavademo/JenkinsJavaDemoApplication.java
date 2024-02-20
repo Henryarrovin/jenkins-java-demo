7f4d3ece126f1b1cfeef136ace57563bee446adc
Henry <henry.a.s.arrovin@gmail.com>
2024-02-20 15:03:21 +0530

    Initial commit

---
+++ src/main/java/com/example/jenkinsjavademo/JenkinsJavaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsjavademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsJavaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsJavaDemoApplication.class, args);
+	}
+
+}
